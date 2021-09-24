9c82f942f374a79147db17d58582a0d95a10d3b1
Tom <tomjuggler@gmail.com>
2021-09-24 21:52:13 +0200

    Initial commit, untested

---
+++ LEDIndicator.ino
@@ -0,0 +1,173 @@
+/***************************************************
+  LED Website Indicator by the Circus Scientist - a visual indicator of visitors to your WordPress webssites
+  This ketch adapted from original code by Adafruit
+  Visit http://ledindicator.devsoft.co.za for more details on how this works
+  This code was tested on the D1 Mini only
+  Original Adafruit notice continues below: 
+
+  **************************************************
+
+  
+  Adafruit MQTT Library ESP8266 Example
+
+  Must use ESP8266 Arduino from:
+    https://github.com/esp8266/Arduino
+
+  Works great with Adafruit's Huzzah ESP board & Feather
+  ----> https://www.adafruit.com/product/2471
+  ----> https://www.adafruit.com/products/2821
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Tony DiCola for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+ 
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+
+#include <WiFiClient.h>
+#define led D4 //built in LED on my D1 mini
+ESP8266WiFiMulti WiFiMulti;
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "Your-SSID"             // replace this with your own WiFi details
+#define WLAN_PASS       "password"              // replace this with your own WiFi details
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "ledmqtt.devsoft.co.za"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL - not set up on the server as yet though
+#define AIO_USERNAME    "asdfasdfas"           // replace this with your own from ledindicator.devsoft.co.za (username)
+#define AIO_KEY         "ghjkghjkgh"           // replace this with your own from ledindicator.devsoft.co.za (password)
+
+/******************************************************* you don't need to change anything below this *************************************************/
+
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiClientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'photocell' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt, "test");
+
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, "LED");
+
+/*************************** Sketch Code ************************************/
+
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH); //HIGH is off for D1 mini
+
+  Serial.println(F("Adafruit MQTT demo"));
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(WLAN_SSID, WLAN_PASS);
+}
+
+uint32_t x = 0;
+
+void loop() {
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+    WiFiClient client;
+    // Setup MQTT subscription for onoff feed.
+    mqtt.subscribe(&onoffbutton);
+    //    HTTPClient http;
+
+    // Ensure the connection to the MQTT server is alive (this will make the first
+    // connection and automatically reconnect when disconnected).  See the MQTT_connect
+    // function definition further below.
+    MQTT_connect();
+
+    // this is our 'wait for incoming subscription packets' busy subloop
+    // try to spend your time here
+
+    Adafruit_MQTT_Subscribe *subscription;
+    while ((subscription = mqtt.readSubscription(5000))) {
+      if (subscription == &onoffbutton) {
+        Serial.print(F("Got: "));
+        Serial.println((char *)onoffbutton.lastread);
+        digitalWrite(led, LOW); //LOW is on for LED_BUILTIN
+        delay(2000);
+        digitalWrite(led, HIGH);
+      }
+
+      // Now we can publish stuff!
+      //  Serial.print(F("\nSending photocell val "));
+      //  Serial.print(x);
+      //  Serial.print("...");
+      //  if (! photocell.publish(x++)) {
+      //    Serial.println(F("Failed"));
+      //  } else {
+      //    Serial.println(F("OK!"));
+      //  }
+
+      // ping the server to keep the mqtt connection alive
+      // NOT required if you are publishing once every KEEPALIVE seconds
+
+      if (! mqtt.ping()) {
+        mqtt.disconnect();
+      } 
+    }
+  }
+}
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+//    Serial.println("tried connecting, but already connected");
+    if (! mqtt.ping()) {
+        mqtt.disconnect();
+      } else {
+//        Serial.println("ping");
+      }
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 8;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0) {
+      // basically die and wait for WDT to reset me
+      while (1);
+    }
+  }
+  Serial.println("MQTT Connected!");
+
+} 
